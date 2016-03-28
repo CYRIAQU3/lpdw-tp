2e445dac383d5cb0c34e9b70d44cf970825a17f2
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-03-28 13:25:13 +0200

    Exercice 5

--- exercice4/header.jsx
+++
@@ -1,9 +0,0 @@
-var App = React.createClass({
-		  render: function () {
-		    return React.createElement('header', {className: "header"},
-        "The Header"
-      );
-		  }
-		});
-
-		React.render(<App name="TP" />, document.getElementById('root'));
--- exercice5/index.js
+++ exercice5/index.js
@@ -1,8 +1,26 @@
-/*import React    from 'react-dom';
+import React    from 'react-dom';
 import ReactDOM from 'react-dom';
 import YourApp  from './components/YourApp';
 
 ReactDOM.render(
   <YourApp />,
   document.getElementById('root')
-);*/
+);
+
+var ip = React.createClass({
+		  render: function() {
+		    return <input type="text"
+		    value={this.state.value}
+        	onChange=dv.setState({val: {this.state.value}}); />;
+		  }
+		});
+
+
+var dv = React.createClass({
+		  render: function () {
+		    return <div id="{this.props.name}">{this.props.val}</div>;
+		  }
+		});
+
+		React.render(<dv name="container_div" val="" />, document.getElementById('root'));
+		React.render(<ip />, document.getElementById('root'));
